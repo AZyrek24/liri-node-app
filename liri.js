fd05fc36ce4a2b5fd43bdaef488f23a611e5abca
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-02 18:41:45 -0700

    added default OMDB functionality

--- liri.js
+++ liri.js
@@ -1,3 +1,6 @@
+//Node Packages
+//=============================================================================================================
+
 var env = require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
@@ -11,6 +14,28 @@ var spotify = new Spotify({
 });
 // var client = new Twitter(keys.twitter);
 console.log(spotify);
-
-//OMDB api url
-// http://www.omdbapi.com/?i=tt3896198&apikey=445dd844
+console.log(process.argv[3]);
+//MOVIE THIS
+if (process.argv[2] === "movie-this" && process.argv[3] === undefined) {
+  //Default OMDB api url if argument left empty
+  request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
+  
+    // If the request is successful (i.e. if the response status code is 200)
+    if (!error && response.statusCode === 200) {
+      // Parse the body of the site
+      console.log("=================================")
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("=================================")
+      console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+      console.log("IMDB: " + JSON.parse(body).Ratings[0].Value);
+      console.log("Country: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+    }
+  });
+}
+// else {
+//   for (var i = 2; i < process.)
+//   var movie = process.argv[3]
+// }
