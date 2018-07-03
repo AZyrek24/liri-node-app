df0ad976c93f1dcc64a2328babbff94cc37449ea
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-02 18:11:12 -0700

    deleted extra .env file

--- liri.js
+++ liri.js
@@ -1,12 +1,15 @@
-require("dotenv").config();
-var keysINeed = require("./keys.js");
+var env = require("dotenv").config();
+var keys = require("./keys.js");
 var request = require("request");
 
 var Spotify = require('node-spotify-api');
 
 //API key information
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+});
+// var client = new Twitter(keys.twitter);
 console.log(spotify);
 
 //OMDB api url
