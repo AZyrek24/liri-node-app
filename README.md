01f0ec2c60015f3aed7065426a3c39f3009181b2
AZyrek24 <zzeppelin2400@gmail.com>
2018-10-23 10:15:19 -0700

    added string interpolation, images to readme.md, npm colors to display

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # LIRI Bot Instructions
 
-# LIRI is a command line node app that takes in parameters and gives back data. There are four different commands which can also be followed by a relevant query.
-# Open terminal and direct to the <liri-node-app> filepath where liri-node-app is located.
-# To Run LIRI, enter one of the following in the command line:
+## LIRI is a command line node app that takes in parameters and gives back data. There are four different commands which can also be followed by a relevant query.
+## Open terminal and direct to the <liri-node-app> filepath where liri-node-app is located.
+## To Run LIRI, enter one of the following in the command line:
   * node liri.js my-tweets
     - This command will log the last 20 tweets of my twitter page
+    ![Image of my-tweets](/assets/images/my-tweets.png)
   
   * node liri.js movie-this *movie-name-here*
     - This command will log your movie's title, RT rating, IMDB rating, country, language, plot summary, and actors from the OMDB API data.
+    ![Image of movie-this](/assets/images/movie-this.png)
   
   * node liri.js spotify-this-song *song-name-here*
     - This command will log data (artist, song's name, preview link, and the album containing the song) from the first matching song in the Spotify API.
+    ![Image of spotify-this-song](/assets/images/spotify-this-song.png)    
   
   * node liri.js do-what-it-says
     - This command will log data from the random.txt file
+    ![Image of do-what-it-says](/assets/images/do-what-it-says.png)  
--- liri.js
+++ liri.js
@@ -1,89 +1,93 @@
 //                                        **** Liri Bot ****
-//=============================================================================================================
+// =============================================================================================================
 //                           **** REFER TO README.MD FILE FOR INSTRUCTIONS ****
 
-//Require and configure dotenv file
-require("dotenv").config();
+// Require and configure dotenv file
+require('dotenv').config();
 
-//Gets keys.js exports
-var keys = require("./keys.js");
+// Colors npm
+var colors = require('colors');
 
-//Node Packages
-//=============================================================================================================
-var fs = require("fs");
-var request = require("request");
-var Twitter = require("twitter");
+// Gets keys.js exports
+var keys = require('./keys.js');
+
+// Node Packages
+// =============================================================================================================
+var fs = require('fs');
+var request = require('request');
+var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 
-//Variables
-//=============================================================================================================
+// Variables
+// =============================================================================================================
 var command = process.argv[2];
-var song = "";
-var movie = "";
+var song = '';
+var movie = '';
 var showData;
 
-//API key information
+// API key information
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-//Switch-Case directs the command to the correct function
+// Switch-Case directs the command to the correct function
 switch (command) {
-  case "my-tweets":
+  case 'my-tweets':
     myTweets();
     break;
 
-  case "movie-this":
+  case 'movie-this':
     movieThis();
     break;
 
-  case "spotify-this-song":
+  case 'spotify-this-song':
     spotifyThisSong();
     break;
 
-  case "do-what-it-says":
+  case 'do-what-it-says':
     doWhatItSays();
     break;
 }
-//Functions
-//=============================================================================================================
+// Functions
+// =============================================================================================================
 
 //                                        **** MOVIE THIS ****
-//=============================================================================================================
+// =============================================================================================================
 function movieThis() {
-  if (command === "movie-this" && process.argv[3] === undefined) {
-    movie = "Mr+Nobody";
+  if (command === 'movie-this' && process.argv[3] === undefined) {
+    movie = 'Mr+Nobody';
     movieDisplay();
   }
-  else if (command === "movie-this") {
+  else if (command === 'movie-this') {
     for (var i = 3; i < process.argv.length; i++) {
-      movie += process.argv[i] + "+";
+      movie += process.argv[i] + '+';
     }
     movieDisplay();
   }
 }
 function movieDisplay() {
-  //OMDB api url 
-  request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+  // OMDB api url 
+  request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
 
     // If the request is successful (i.e. if the response status code is 200)
     if (!error && response.statusCode === 200) {
       // Parse the body of the site
-      console.log("\n=================================")
-      console.log("Title: " + JSON.parse(body).Title);
-      console.log("=================================")
-      console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
-      console.log("\nIMDB: " + JSON.parse(body).Ratings[0].Value);
-      console.log("\nCountry: " + JSON.parse(body).Country);
-      console.log("\nLanguage: " + JSON.parse(body).Language);
-      console.log("\nPlot: " + JSON.parse(body).Plot);
-      console.log("\nActors: " + JSON.parse(body).Actors);
-      console.log("=================================\n")
+      prsBody = JSON.parse(body);
+      console.log(`\n==================================================================`.yellow +
+                  `\n                       Title: ${prsBody.Title}`.yellow +
+                  `\n==================================================================\n`.yellow +
+                  `\nRotten Tomatoes:`.blue + ` ${prsBody.Ratings[1].Value}\n` +
+                  `\nIMDB:`.blue + ` ${prsBody.Ratings[0].Value}\n` +
+                  `\nCountry:`.blue + ` ${prsBody.Country}\n` +
+                  `\nLanguage:`.blue + ` ${prsBody.Language}\n` +
+                  `\nPlot:`.blue + ` ${prsBody.Plot}\n` +
+                  `\nActors:`.blue + ` ${prsBody.Actors}\n` +
+                  `==================================================================\n`.yellow);
     }
   });
 }
 
 //                                          **** MY TWEETS ****
-//=============================================================================================================
+// =============================================================================================================
 function myTweets() {
   var params = { screen_name: '@Phineas05526995', count: 20 };
 
@@ -91,13 +95,14 @@ function myTweets() {
     // If the request is successful
     if (!error) {
       // Parse the text of the tweets
-      console.log("\n=================================")
-      console.log("LAST 20 TWEETS");
-      console.log("=================================");
+      console.log(`\n==================================================================`.yellow +
+                  `\n                        LAST 20 TWEETS`.yellow +
+                  `\n==================================================================\n`.yellow);
       for (var i = 0; i < 20; i++) {
-        console.log((i + 1) + ". " + tweets[i].text);
+        var num = i + 1;
+        console.log(`${num}`.blue + `. ${tweets[i].text}`);
       }
-      console.log("=================================\n");
+      console.log(`==================================================================\n`.yellow);
     }
     else {
       throw error;
@@ -106,15 +111,15 @@ function myTweets() {
 }
 
 //                                     **** SPOTIFY THIS SONG ****
-//=============================================================================================================
+// =============================================================================================================
 function spotifyThisSong() {
-  if (command === "spotify-this-song" && process.argv[3] === undefined) {
-    song = "The Sign";
+  if (command === 'spotify-this-song' && process.argv[3] === undefined) {
+    song = 'The Sign';
     songDisplay();
   }
-  else if (command === "spotify-this-song") {
+  else if (command === 'spotify-this-song') {
     for (var i = 3; i < process.argv.length; i++) {
-      song += process.argv[i] + "+";
+      song += process.argv[i] + '+';
     }
     songDisplay();
   }
@@ -122,41 +127,42 @@ function spotifyThisSong() {
 function songDisplay() {
   spotify.search({ type: 'track', query: song }, function (err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      return console.log('Error occu: ' + err);
     }
-    console.log("\n=================================");
-    console.log("Artist: " + data.tracks.items[0].artists[0].name);
-    console.log("=================================");
-    console.log("\nSong Name: " + data.tracks.items[0].name);
-    console.log("\nSpotify Link: " + data.tracks.items[0].preview_url);
-    console.log("\nAlbum: " + data.tracks.items[0].album.name);
-    console.log("=================================");
+    var items = data.tracks.items;
+    console.log(`\n==================================================================\n`.yellow +
+                `                       Artist: ${items[0].artists[0].name}\n`.yellow +
+                `==================================================================\n`.yellow +
+                `\nSong Name:`.blue + ` ${items[0].name}\n` +
+                `\nSpotify Link:`.blue + ` ${items[0].preview_url}\n` +
+                `\nAlbum:`.blue + ` ${items[0].album.name}\n`+
+                `\n==================================================================\n`.yellow);
   });
 }
 
 //                                     **** DO WHAT IT SAYS ****
-//=============================================================================================================
+// =============================================================================================================
 
 function doWhatItSays() {
 
-  fs.readFile("random.txt", "utf8", function (error, data) {
+  fs.readFile('random.txt', 'utf8', function (error, data) {
     if (error) {
       return console.log(error);
     }
-    var dataArr = data.split(",");
+    var dataArr = data.split(',');
     command = dataArr[0];
     var argument = dataArr[1];
     switch (command) {
-      case "my-tweets":
+      case 'my-tweets':
         myTweets();
         break;
 
-      case "movie-this":
+      case 'movie-this':
         movie = argument;
         movieDisplay();
         break;
 
-      case "spotify-this-song":
+      case 'spotify-this-song':
         song = argument;
         songDisplay();
         break;
